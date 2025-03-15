5bd1c45714fab7039a8aa5b9fa5c98497ae7fb3d
Methminpulsara <methminpulsara10@gmail.com>
2025-03-16 03:37:51 +0530

    update

--- src/main/java/edu/icet/ecom/dto/User.java
+++ src/main/java/edu/icet/ecom/dto/User.java
@@ -1,4 +1,17 @@
 package edu.icet.ecom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Transactional
 public class User {
+
+    private Integer id ;
+    private String name;
+    private String city;
 }
--- src/main/java/edu/icet/ecom/entity/UserEntity.java
+++ src/main/java/edu/icet/ecom/entity/UserEntity.java
@@ -1,4 +1,25 @@
 package edu.icet.ecom.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name= "user")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id ;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
 }
