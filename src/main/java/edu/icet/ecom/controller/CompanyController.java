8f545dfc5b72224ee1b6fdaecdda14e1821fa2fd
Methminpulsara <methminpulsara10@gmail.com>
2025-04-01 20:52:50 +0530

    update

--- src/main/java/edu/icet/ecom/controller/CompanyController.java
+++ src/main/java/edu/icet/ecom/controller/CompanyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/company")
+@RequestMapping("/api/company")
 @CrossOrigin
 @RequiredArgsConstructor
 
